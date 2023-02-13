3852a71aacc1737f9eaf1d7946883f8814ac02e2
ﾓｽﾞｸ <116736861+precious-m02uku@users.noreply.github.com>
2023-02-13 17:42:27 +0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+const input = document.getElementById('input');
+const output = document.getElementById('output');
+
+function submitName() {
+    output.innerText = input.value + "、きみはほんとうにすごいね";
+}
